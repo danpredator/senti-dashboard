5dbd3aa41592b4b45917944c13cf680cc314ae4a
danpredator <danpredator@users.noreply.github.com>
2020-07-14 01:05:23 +0530

    removed wanted , nav bar created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2">
+          <span className="navbar-brand col-sm-3 col-md-2 mr-0" >Sentimental Analysis</span>
+          <input className="form-control form-control-dark w-70" type="text" placeholder="Search" aria-label="Search" />
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap">
+              
+            </li>
+          </ul>
+        </nav>
+        <div style={{ backgroundColor: 'blue'}}>
+          </div>        
     </div>
-  );
-}
 
+    );
+  }
+}
 export default App;
